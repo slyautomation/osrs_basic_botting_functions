1ec873c22fe89be75272ccb645ac9c3cebf13df1
slyautomation <81003470+slyautomation@users.noreply.github.com>
2021-05-22 05:12:50 +1000

    Update core.py
    Follow the new setup guide to remove the title from the runelite header:  https://youtu.be/JO2FvkJwppA

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ import win32gui
 global hwnd
 hwnd = 0
 
-def findWindow_runelite(Name):  # find window name returns PID of the window
+def legacy_findWindow_runelite(Name):  # find window name returns PID of the window
     global hwnd
     hwnd = win32gui.FindWindow(None, "RuneLite - " + Name)
     #hwnd = win32gui.GetForegroundWindow()
@@ -11,4 +11,13 @@ def findWindow_runelite(Name):  # find window name returns PID of the window
     #win32gui.ShowWindow(hwnd)
     win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
 
-findWindow_runelite('user name')
+def findWindow_runelite():  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, "RuneLite") # follow the setup guide to remove the username in the title using the runelite settings
+    #hwnd = win32gui.GetForegroundWindow() # link is: https://youtu.be/JO2FvkJwppA
+    print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    #win32gui.ShowWindow(hwnd)
+    win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
+
+findWindow_runelite()
