c2ee6e8b7d05ab48c3378c116da1d543ce772612
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2021-09-22 15:55:08 +1000

    Update combat.py

--- combat.py
+++ combat.py
@@ -5,12 +5,12 @@ import random
 import time
 import argparse
 import os
-
+import core
 global hwnd
 global iflag
 global icoord
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageGrab
 from functions import Image_to_Text
 from functions import resizeImage
 from functions import random_combat
@@ -22,7 +22,7 @@ from functions import image_Rec_clicker
 # from core import findWindow_runelite
 
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 iflag = False
 
 global newTime_break
@@ -71,10 +71,19 @@ def randomizer(timer_breaks, ibreaks):
     if newTime_break == True:
         timer_break = timer()
         ibreak = random.randrange(600, 2000)
-        newTime_break = False
+
+         newTime_break = False
 
     # b = random.uniform(4, 5)
 
+def McropImage_quick():
+    left = 150
+    top = 150
+    right = 600
+    bottom = 750
+
+    im = ImageGrab.grab(bbox=(left, top, right, bottom))
+    im.save('screenshot2.png', 'png')
 
 def McropImage():
     myScreenshot = pyautogui.screenshot()
@@ -89,6 +98,58 @@ def McropImage():
     im = im.crop((left, top, right, bottom))  # defines crop points
     im.save(r"screenshot2.png")  # saves new cropped image
 
+def findarea_attack_quick(object, deep=20):
+    McropImage_quick()
+    image = cv2.imread(r"screenshot2.png")
+
+    # B, G, R
+    # --------------------- ADD OBJECTS -------------------
+    red = ([0, 0, 180], [80, 80, 255])
+    green = ([0, 180, 0], [80, 255, 80])
+    pickup_high = ([200, 0, 100], [255, 30, 190])
+    attack_blue = ([250, 250, 0], [255, 255, 5])
+    amber = ([0, 160, 160], [80, 255, 255])
+    # --------------------- ADD OBJECTS -------------------
+    ore_list = [red, green, pickup_high, attack_blue, amber]
+    boundaries = [ore_list[object]]
+    # loop over the boundaries
+    for (lower, upper) in boundaries:
+        # create NumPy arrays from the boundaries
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+        # find the colors within the specified boundaries and apply
+        # the mask
+        mask = cv2.inRange(image, lower, upper)
+        output = cv2.bitwise_and(image, image, mask=mask)
+        ret, thresh = cv2.threshold(mask, 40, 255, 0)
+        # if (cv2.__version__[0] > 3):
+        # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # else:
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if len(contours) != 0:
+            # draw in blue the contours that were founded
+            # cv2.drawContours(output, contours, -1, 255, 3)
+            # find the biggest countour (c) by the area
+            c = max(contours, key=cv2.contourArea)
+
+            x, y, w, h = cv2.boundingRect(c)
+            # draw the biggest contour (c) in green
+            whalf = max(round(w / 2), 1)
+            hhalf = max(round(h / 2), 1)
+
+            # cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            x = random.randrange(x + 150 + whalf - deep, x + 150 + max(whalf + deep, 1))  # 950,960
+            print('attack x: ', x)
+            y = random.randrange(y + 150 + hhalf - deep, y + 150 + max(hhalf + deep, 1))  # 490,500
+            print('attack y: ', y)
+            b = random.uniform(0.05, 0.1)
+            pyautogui.moveTo(x, y, duration=b)
+            b = random.uniform(0.01, 0.05)
+            pyautogui.click(duration=b)
+            return True
+    return False
+    # show the images
+    # cv2.imshow("Result", np.hstack([image, output]))
 
 def findarea_attack(object, deep=20):
     McropImage()
@@ -169,7 +230,7 @@ def powerattack_text(monster='chicken', burybones=True, Take_Human_Break=False, 
                 if findarea_attack(2, 5):  # pick up highlighted loot
                     c = random.uniform(3, 5)
                     time.sleep(c)
-                if findarea_attack(3):  # attack npc/monster
+                if findarea_attack_quick(3):  # attack npc/monster
                     c = random.uniform(3, 5)
                     time.sleep(c)
                     if Take_Human_Break:
@@ -178,6 +239,7 @@ def powerattack_text(monster='chicken', burybones=True, Take_Human_Break=False, 
 
 
 if __name__ == "__main__":
+    McropImage_quick()
     # do whatever you do
     # ----- UPDATE WITH ALL VARIATIONS OF MONSTER'S IMAGE TO TEXT RESULT IN LINE WITH MONSTER_LIST -----
     monster_array = [
@@ -193,4 +255,4 @@ if __name__ == "__main__":
     ibreak = random.randrange(300, 2000)
     print('will break in   ' + str(ibreak / 60) + ' minutes')
     timer_break = timer()
-    powerattack_text('cow', Take_Human_Break=True, Run_Duration_hours=4)
+    powerattack_text('chicken', Take_Human_Break=False, Run_Duration_hours=4)
