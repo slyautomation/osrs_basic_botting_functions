f8c67c92dbda71366cb47a3c1338fb2c9bad7149
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2021-10-26 13:22:45 +1100

    Update core.py
    added find window in alignment with yaml config file

--- core.py
+++ core.py
@@ -1,8 +1,11 @@
 import win32gui
+import yaml
 
 global hwnd
 hwnd = 0
 
+with open("pybot-config.yaml", "r") as yamlfile:
+    data = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
 def findWindow_runelite():  # find window name returns PID of the window
     global hwnd
@@ -13,5 +16,22 @@ def findWindow_runelite():  # find window name returns PID of the window
     # win32gui.ShowWindow(hwnd)
     win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
 
+def findWindow_openosrs():  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, "OpenOSRS")
+    # hwnd = win32gui.GetForegroundWindow()860
+    print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    # win32gui.ShowWindow(hwnd)
+    win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
 
-findWindow_runelite()
+def findWindow(data):  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, data)
+    # hwnd = win32gui.GetForegroundWindow()860
+    print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    # win32gui.ShowWindow(hwnd)
+    win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
+
+findWindow(data[0]['Config']['client_title'])
