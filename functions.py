e150e71d0cbeb57645d4df0fe760797703bc4a83
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2021-10-26 14:20:08 +1100

    Update functions.py

--- functions.py
+++ functions.py
@@ -485,6 +485,69 @@ def mini_map_image(image, iwidth=0, iheight=0, threshold=0.7, clicker='left', xs
         pyautogui.click(icoord, duration=b, button=clicker)
     return iflag
 
+def McropImage_quick():
+    left = 150
+    top = 150
+    right = 600
+    bottom = 750
+
+    im = ImageGrab.grab(bbox=(left, top, right, bottom))
+    im.save('screenshot2.png', 'png')
+
+
+def findarea_attack_quick(object, deep=20):
+    McropImage_quick()
+    image = cv2.imread(r"screenshot2.png")
+
+    # B, G, R
+    # --------------------- ADD OBJECTS -------------------
+    red = ([0, 0, 180], [80, 80, 255])
+    green = ([0, 180, 0], [80, 255, 80])
+    pickup_high = ([200, 0, 100], [255, 30, 190])
+    attack_blue = ([250, 250, 0], [255, 255, 5])
+    amber = ([0, 160, 160], [80, 255, 255])
+    # --------------------- ADD OBJECTS -------------------
+    ore_list = [red, green, pickup_high, attack_blue, amber]
+    boundaries = [ore_list[object]]
+    # loop over the boundaries
+    for (lower, upper) in boundaries:
+        # create NumPy arrays from the boundaries
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+        # find the colors within the specified boundaries and apply
+        # the mask
+        mask = cv2.inRange(image, lower, upper)
+        output = cv2.bitwise_and(image, image, mask=mask)
+        ret, thresh = cv2.threshold(mask, 40, 255, 0)
+        # if (cv2.__version__[0] > 3):
+        # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # else:
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        if len(contours) != 0:
+            # draw in blue the contours that were founded
+            # cv2.drawContours(output, contours, -1, 255, 3)
+            # find the biggest countour (c) by the area
+            c = max(contours, key=cv2.contourArea)
+
+            x, y, w, h = cv2.boundingRect(c)
+            # draw the biggest contour (c) in green
+            whalf = max(round(w / 2), 1)
+            hhalf = max(round(h / 2), 1)
+
+            # cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            x = random.randrange(x + 150 + whalf - deep, x + 150 + max(whalf + deep, 1))  # 950,960
+            print('attack x: ', x)
+            y = random.randrange(y + 150 + hhalf - deep, y + 150 + max(hhalf + deep, 1))  # 490,500
+            print('attack y: ', y)
+            b = random.uniform(0.05, 0.1)
+            pyautogui.moveTo(x, y, duration=b)
+            b = random.uniform(0.01, 0.05)
+            pyautogui.click(duration=b)
+            return True
+    return False
+    # show the images
+    # cv2.imshow("Result", np.hstack([image, output]))
+    
 def mini_map_bool(image, threshold=0.7):
     screen_Image(1941 - 1280, 27, 2106 - 1280, 190, 'mini_map.png')
     global icoord
@@ -513,8 +576,17 @@ def mini_map_bool(image, threshold=0.7):
         iflag = True
     return iflag
 
+def xp_quick():
+    left = 560
+    top = 95
+    right = 595
+    bottom = 135
+
+    im = ImageGrab.grab(bbox=(left, top, right, bottom))
+    im.save('xp_gain.png', 'png')
+
 def xp_gain_check(image, threshold=0.5):
-    screen_Image(1825 - 1280, 75, 1890 - 1280, 190, 'xp_gain.png')
+    xp_quick()
     global iflag
     img_rgb = cv2.imread('xp_gain.png')
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
@@ -529,6 +601,21 @@ def xp_gain_check(image, threshold=0.5):
     if pt is None:
         iflag = False
     return iflag
+
+def bank_ready(deposit_second=True):
+    bank = Image_count('bank_deposit.png', 0.75)
+    print("bank deposit open:", bank)
+    if bank > 0:
+        if deposit_second:
+            deposit_secondItem()
+        return True
+    else:
+        return False
+    return False
+
+def invent_enabled():
+    return Image_count('inventory_enabled.png', threshold=0.9)
+
 
 def Image_Rec_single(image, event, iheight, iwidth, threshold, clicker, ispace=20, cropx=0, cropy=0, playarea=True):
     global icoord
