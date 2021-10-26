14aeafd8b7e3ea564886b8759d9f36860b8e345d
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2021-10-27 09:40:27 +1100

    Update core.py

--- core.py
+++ core.py
@@ -33,5 +33,24 @@ def findWindow(data):  # find window name returns PID of the window
     win32gui.SetActiveWindow(hwnd)
     # win32gui.ShowWindow(hwnd)
     win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
+    
+def getWindow(data):  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, data)
+    # hwnd = win32gui.GetForegroundWindow()860
+    print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    win32gui.SetForegroundWindow(hwnd)
+    rect = win32gui.GetWindowRect(hwnd)
+    # Take care of borders for finding character
+    x = rect[0]
+    # 30 is the amount of pixels in the client top border
+    y = rect[1] + 30
+    # 50 is the amount of pixels in the client side border
+    w = rect[2] - x - 50
+    h = rect[3] - y - 30
+    # Find center
+    print('window width:', w, 'window height:', h)
+    return x, y, w, h
 
 findWindow(data[0]['Config']['client_title'])
