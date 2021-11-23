54ebf414959ddeccf68900babba52a4188a28300
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2021-11-23 18:22:14 +1100

    Update core.py

--- core.py
+++ core.py
@@ -1,12 +1,34 @@
 import win32gui
 import yaml
-
+import platform
 global hwnd
 hwnd = 0
 
 with open("pybot-config.yaml", "r") as yamlfile:
     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    
+def findWindow_Linux(data)
+    import subprocess
+    subprocess.call(["xdotool", "search", "--name", data, "windowfocus", "%2"])
+    subprocess.call(["xdotool", "getwindowfocus", "windowmove", "0", "0"])
+    subprocess.call(["xdotool", "getwindowfocus", "windowsize", "860", "830"])
 
+    
+def getWindow_Linux(data)
+    import subprocess
+    subprocess.call(["xdotool", "search", "--name", data, "windowfocus", "%2"])
+    rect = subprocess.call(["xdotool", "getwindowfocus", "getwindowgeometry"])
+    # Take care of borders for finding character
+    x = rect[0]
+    # 30 is the amount of pixels in the client top border
+    y = rect[1] + 30
+    # 50 is the amount of pixels in the client side border
+    w = rect[2] - x - 50
+    h = rect[3] - y - 30
+    # Find center
+    print('window width:', w, 'window height:', h)
+    return x, y, w, h
+    
 def findWindow_runelite():  # find window name returns PID of the window
     global hwnd
     hwnd = win32gui.FindWindow(None, "RuneLite")
@@ -53,4 +75,8 @@ def getWindow(data):  # find window name returns PID of the window
     print('window width:', w, 'window height:', h)
     return x, y, w, h
 
-findWindow(data[0]['Config']['client_title'])
+print('Operating system:', platform.system())
+if platform.system() == 'Linux':
+    findWindow_Linux(data[0]['Config']['client_title'])
+else:
+    findWindow(data[0]['Config']['client_title'])
