253bd031464c40f7b1864c821523110ed3a566a1
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2023-05-11 21:33:04 +1000

    Update Air_Craft.py

--- Air_Craft.py
+++ Air_Craft.py
@@ -4,9 +4,11 @@ import pyautogui
 import random
 import time
 
+import win32gui
 import yaml
 
-from core import getWindow
+import core
+from core import findWindow
 
 global hwnd
 global iflag
@@ -20,7 +22,9 @@ iflag = False
 with open("pybot-config.yaml", "r") as yamlfile:
     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
-x_win, y_win, w_win, h_win = getWindow(data[0]['Config']['client_title'])
+
+
+x_win, y_win, w_win, h_win = core.getWindow('RuneLite')
 
 def determine_position_to_bank():
     print('determining position to bank')
@@ -57,7 +61,7 @@ def determine_position_to_bank():
         return 0
     if mini_map_bool('air_craft_mark_7.png', 0.7):
         print('player located @ step 6_2')
-        make_runes()
+        make_runes_at_alter()
         print("to bank")
         return 2
     if mini_map_bool('air_craft_mark_8.png', 0.7):
@@ -112,12 +116,12 @@ def determine_position_to_airalter():
     if mini_map_bool('air_craft_mark_6.png', 0.7):
         print('player located @ step 6_2')
         print("to air alter")
-        make_runes()
+        make_runes_at_alter()
         return 8
     if mini_map_bool('air_craft_mark_7.png', 0.7):
         print('player located @ air alter')
         print("to air alter")
-        make_runes()
+        make_runes_at_alter()
         return 8
     if mini_map_bool('air_craft_mark_9.png', 0.7):
         print('player located @ step 6_3')
@@ -131,13 +135,11 @@ def determine_position_to_airalter():
     return 0
 
 
-def invent_enabled():
-    return Image_count('inventory_enabled.png', threshold=0.9)
-
 
 def to_air_craft():
+    global bank_runes_position_x, bank_runes_position_y
     step = 0
-    invent = invent_enabled()
+    invent = functions.invent_enabled()
     print(invent)
     if invent == 0:
         pyautogui.press('esc')
@@ -152,16 +154,14 @@ def to_air_craft():
         print('bank booth')
         c = random.uniform(6.5, 8.5)
         time.sleep(c)
-        pick_item(1770 - 1280, 635)
-        pick_item(1655 - 1280, 194)
-        exit_bank()
+        withdraw_bank_runes(bank_runes_position_x, bank_runes_position_y)
 
     if step == 0:
         while mini_map_image('air_craft_bank.png', -5, -5, 0.7, 'left', 15, 10) == False:
             mini_map_image('air_craft_bank.png', 0, 0, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_bank.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 1 to air alter spot not found")
         print("step 1 to air alter")
         random_breaks(5, 8)
@@ -172,7 +172,7 @@ def to_air_craft():
             mini_map_image('air_craft_bank.png', 0, 40, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_bank.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 2 to air alter spot not found")
         print("step 2 to air alter")
         random_breaks(8, 10)
@@ -183,7 +183,7 @@ def to_air_craft():
             mini_map_image('air_craft_mark_1.png', 10, 40, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_1.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 3 to air alter not found")
         print("step 3 to air alter")
         random_breaks(5, 8)
@@ -194,7 +194,7 @@ def to_air_craft():
             mini_map_image('air_craft_mark_2.png', 10, 40, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_2.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 4 to air alter not found")
         print("step 4 to air alter")
         random_breaks(5, 8)
@@ -205,7 +205,7 @@ def to_air_craft():
             mini_map_image('air_craft_mark_3.png', 0, 40, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_3.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 5 to air alter not found")
         print("step 5 to air alter")
         random_breaks(5, 8)
@@ -216,7 +216,7 @@ def to_air_craft():
             mini_map_image('air_craft_mark_4.png', 0, 15, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_4.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 6 to air alter not found")
         print("step 6 to air alter")
         random_breaks(5, 8)
@@ -227,7 +227,7 @@ def to_air_craft():
             mini_map_image('air_craft_mark_4.png', 0, 40, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_4.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 7 to air alter not found")
         print("step 7 to air alter")
         random_breaks(5, 8)
@@ -238,7 +238,7 @@ def to_air_craft():
             mini_map_image('air_craft_mark_5.png', -20, 20, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_5.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 8 to air alter not found")
         print("step 8 to air alter")
         random_breaks(12, 15)
@@ -246,8 +246,9 @@ def to_air_craft():
 
 
 def to_bank():
+    global bank_runes_position_x, bank_runes_position_y
     step = 0
-    invent = invent_enabled()
+    invent = functions.invent_enabled()
     print(invent)
     if invent == 0:
         pyautogui.press('esc')
@@ -255,14 +256,14 @@ def to_bank():
     if runes == 0 or runes == 1:
         step = determine_position_to_bank()
     else:
-        make_runes()
+        make_runes_at_alter()
 
     if step == 0:
         while mini_map_image('air_craft_mark_6.png', 40, -10, 0.7, 'left', 15, 10) == False:
             mini_map_image('air_craft_mark_6.png', 40, -10, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_6.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 1 to bank not found")
         print("step 1 to bank")
         random_breaks(10, 13)
@@ -273,30 +274,39 @@ def to_bank():
             mini_map_image('air_craft_mark_5.png', 0, -25, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_5.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 2 to bank not found")
         print("step 2 to bank")
         random_breaks(8, 10)
         mini_map_image('air_craft_mark_5.png', 0, -55, 0.7, 'left', 15, 10)
         random_breaks(5, 8)
         step = 2
+
     if step == 2:
-        while mini_map_image('air_craft_mark_4.png', 30, -15, 0.7, 'left', 15, 10) == False:
-            mini_map_image('air_craft_mark_4.png', 30, -15, 0.7, 'left', 15, 10)
-            if mini_map_bool('air_craft_mark_4.png', 0.7) == False:
-                print("air alter not found")
-                making_air_runes()
+        time_start = time.time()
+        time_end = 0
+        c = random.uniform(12.5, 14.5)
+        step2 = mini_map_image('air_craft_mark_4.png', 30, -15, 0.7, 'left', 15, 10)
+        while step2 == False and time_end < c:
+            time_end = time.time() - time_start
+            time.sleep(0.3)
+            step2 = mini_map_image('air_craft_mark_4.png', 30, -15, 0.7, 'left', 15, 10)
+            print(step2)
+        if step2 == False:
+            print("air alter not found")
+            runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 3 to bank not found")
-        print("step 3 to bank")
-        random_breaks(8, 11)
-        step = 3
+        else:
+            print("step 3 to bank")
+            random_breaks(8, 11)
+            step = 3
 
     if step == 3:
         while mini_map_image('air_craft_mark_3.png', 5, -10, 0.7, 'left', 15, 10) == False:
             mini_map_image('air_craft_mark_3.png', 5, -10, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_3.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 4 to bank not found")
         print("step 4 to bank")
         random_breaks(5, 8)
@@ -307,7 +317,7 @@ def to_bank():
             mini_map_image('air_craft_mark_2.png', 10, -10, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_2.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 5 to bank not found")
         print("step 5 to bank")
         random_breaks(5, 8)
@@ -318,7 +328,7 @@ def to_bank():
             mini_map_image('air_craft_mark_2.png', 5, -25, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_2.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 6 to bank not found")
         print("step 6 to bank")
         random_breaks(5, 8)
@@ -329,7 +339,7 @@ def to_bank():
             mini_map_image('air_craft_mark_1.png', 5, -15, 0.7, 'left', 15, 10)
             if mini_map_bool('air_craft_mark_1.png', 0.7) == False:
                 print("air alter not found")
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 7 to bank not found")
         print("step 7 to bank")
         random_breaks(5, 8)
@@ -342,41 +352,14 @@ def to_bank():
                 print("air alter not found")
                 functions.find_Object(2, left=0, top=0, right=w_win, bottom=h_win)
                 random_breaks(8, 10)
-                making_air_runes()
+                runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
             print("step 8 to bank not found")
         print("step 8 to bank")
         random_breaks(8, 10)
         step = 8
 
 
-def exit_bank():
-    print('exit bank')
-    c = random.uniform(0.3, 0.7)
-    x = random.randrange(523, 540)
-    print('x: ', x)
-    y = random.randrange(40, 55)
-    print('y: ', y)
-    b = random.uniform(0.15, 0.5)
-    pyautogui.moveTo(x, y, duration=b)
-    b = random.uniform(0.1, 0.19)
-    pyautogui.click(duration=b, button='left')
-    time.sleep(c)
-
-
-def pick_item(v, u):
-    c = random.uniform(0.3, 0.7)
-    d = random.uniform(0.05, 0.15)
-    x = random.randrange(v - 10, v + 10)
-    print('x: ', x)
-    y = random.randrange(u - 5, u + 5)
-    b = random.uniform(0.2, 0.6)
-    pyautogui.moveTo(x, y, duration=b)
-    time.sleep(d)
-    pyautogui.click(button='left')
-    time.sleep(c)
-
-
-def make_runes():
+def make_runes_at_alter():
     if mini_map_bool('air_craft_mark_7.png', 0.7) == False:
         functions.find_Object(2, left=0, top=0, right=w_win, bottom=h_win)
         c = random.uniform(1.5, 3)
@@ -401,7 +384,21 @@ def count_runes():
     return Image_count('rune_icon.png', threshold=0.7)
 
 
-def making_air_runes():
+def withdraw_bank_runes(rune_x,rune_y):
+    error_c = 0
+    while functions.bank_ready(False) == False:
+        if error_c > 3:
+            exit()
+        functions.find_Object(1, left=0, top=0, right=w_win, bottom=h_win) # mark / highlight object marker for the bank booth - GREEN
+        c = random.uniform(3, 6)
+        time.sleep(c)
+        error_c += 1
+    print('bank booth')
+    functions.deposit_all_Bank()
+    functions.pick_item(rune_x, rune_y)
+    functions.exit_bank(Debug=False)
+
+def runecrafting_air_runes(bankrune_x=185,bankrune_y=305):
     invent = functions.invent_enabled()
     if invent == 0:
         actions = 'opening inventory'
@@ -414,30 +411,27 @@ def making_air_runes():
     runes = count_runes()
     if runes > 1:
         print("player has runes to craft")
-        functions.find_Object(2, left=0, top=0, right=w_win, bottom=h_win)
+        functions.find_Object(2, left=0, top=0, right=w_win, bottom=h_win) # mark / highlight object marker for the entrance to air alter - YELLOW
         print('air alter')
         c = random.uniform(6.5, 8.5)
         time.sleep(c)
-        make_runes()
+        make_runes_at_alter()
     b = 1  # random.randrange(1, 3)
-    functions.Image_Rec_single('activity_adviser.png', 'close activity advisor', iheight=5, iwidth=120, threshold=0.9, ispace=10, playarea=True)
     options = {1: to_bank
                }
     options[b]()
-    functions.find_Object(1, left=0, top=0, right=w_win, bottom=h_win)
-    print('bank booth')
-    c = random.uniform(6.5, 8.5)
-    time.sleep(c)
-    pick_item(1770 - 1280, 635)
-    pick_item(1655 - 1280, 194)
+    withdraw_bank_runes(bankrune_x, bankrune_y) # mark / highlight object marker for the bank booth at falador - GREEN
 
-    exit_bank()
 
+
+bank_runes_position_x = 375
+bank_runes_position_y = 123
+Run_Duration_Hours = 4
 
 if __name__ == "__main__":
     x = random.randrange(100, 250)
     y = random.randrange(400, 500)
     pyautogui.click(x, y, button='right')
-    j = 0
-    while j < 10:
-        making_air_runes()
+    t_end = time.time() + (60 * 60 * Run_Duration_Hours)
+    while time.time() < t_end:
+        runecrafting_air_runes(bank_runes_position_x, bank_runes_position_y)
