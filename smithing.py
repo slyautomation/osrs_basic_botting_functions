f5988cf5248f3b4b3a6e57c9f1e13a1c70185a6d
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2023-06-02 09:46:09 +1000

    Update smithing.py

--- smithing.py
+++ smithing.py
@@ -15,7 +15,15 @@ global hwnd, iflag, icoord, newTime_break, \
 iflag = False
 newTime_break = False
 
-
+def findWindow(data):  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, data)
+    # hwnd = win32gui.GetForegroundWindow()860
+    #print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    # win32gui.ShowWindow(hwnd)
+    win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
+    
 def random_break(start, c):
     global newTime_break
     startTime = time.time()
