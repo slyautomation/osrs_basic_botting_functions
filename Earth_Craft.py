0046638e31af95fd2828c5db0bb66564e72db369
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2023-06-02 09:38:36 +1000

    Update Earth_Craft.py

--- Earth_Craft.py
+++ Earth_Craft.py
@@ -12,9 +12,19 @@ global hwnd
 global iflag
 global icoord
 from PIL import Image
-# from matplotlib import pyplot as plt
 
 iflag = False
+# from matplotlib import pyplot as plt
+def findWindow(data):  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, data)
+    # hwnd = win32gui.GetForegroundWindow()860
+    #print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    # win32gui.ShowWindow(hwnd)
+    win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
+
+
 def RcropImage():
     myScreenshot = pyautogui.screenshot()
     myScreenshot.save(r"screen.png")
