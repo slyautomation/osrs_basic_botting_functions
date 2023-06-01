1dc5419894bb6c32f17c23479cc08bebc535575a
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2023-06-02 09:46:38 +1000

    Update woodcutting.py

--- woodcutting.py
+++ woodcutting.py
@@ -51,6 +51,15 @@ class bcolors:
     FAIL = '\033[91m' #RED
     RESET = '\033[0m' #RESET COLOR
 
+def findWindow(data):  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, data)
+    # hwnd = win32gui.GetForegroundWindow()860
+    #print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    # win32gui.ShowWindow(hwnd)
+    win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
+    
 def random_break(start, c):
     global newTime_break
     startTime = time.time()
