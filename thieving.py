2aeb7a71b330ad9f4e2cced6d388edea35c9db31
Sly Automation <81003470+slyautomation@users.noreply.github.com>
2023-06-02 09:46:21 +1000

    Update thieving.py

--- thieving.py
+++ thieving.py
@@ -17,6 +17,15 @@ iflag = False
 icoord = []
 from functions import image_Rec_inventory
 
+def findWindow(data):  # find window name returns PID of the window
+    global hwnd
+    hwnd = win32gui.FindWindow(None, data)
+    # hwnd = win32gui.GetForegroundWindow()860
+    #print('findWindow:', hwnd)
+    win32gui.SetActiveWindow(hwnd)
+    # win32gui.ShowWindow(hwnd)
+    win32gui.MoveWindow(hwnd, 0, 0, 865, 830, True)
+    
 def skill_lvl_up():
     counter = 0
     myScreenshot = pyautogui.screenshot()
